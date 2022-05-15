f6365f4365b8816dd6558bde84ddd7706bd2afb1
Timcp189 <93195503+Timcp189@users.noreply.github.com>
2022-05-15 22:19:47 +0100

    Update RunBuilder.py

--- RunBuilder.py
+++ RunBuilder.py
@@ -1,11 +1,17 @@
+# import modules to build RunBuilder helper classes
 from itertools import product
 from collections import namedtuple
 
-
+# helper classes added to manage the hyperparams & training process
+# read in the hyper params and return a Run namedtuple containing all the 
+# combinations of hyper-parameters
 class RunBuilder():
     @staticmethod
     def get_runs(params):
-
+      """
+      Takes the params dictionary which we define in the 'params' argument. 
+      And takes every parameter with every other parameter.
+      """
         Run = namedtuple('Run', params.keys())
 
         runs = []
