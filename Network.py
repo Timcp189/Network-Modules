7ef4d2a030091e2e8d4423d27ee73f036d2e6896
Timcp189 <93195503+Timcp189@users.noreply.github.com>
2022-05-15 22:19:08 +0100

    Update Network.py

--- Network.py
+++ Network.py
@@ -1,34 +1,44 @@
+# import standard PyTorch modules 
 import torch
 import torch.nn as nn
 import torch.nn.functional as f
-from collections import OrderedDict
+from collections import OrderedDict 
 
 class Network(nn.Module):
+    """
+    Convolution Block
+    """
 
-    def __init__(self, in_ch, mid_ch, out_ch, kernel_size, stride, groups1, groups2):
+    def __init__(self, in_ch, mid_ch, out_ch, kernel_size):
         super(Network, self).__init__()
 
         self.conv_layers = nn.Sequential(OrderedDict([
-            ('conv1', nn.Conv2d(in_channels=in_ch, out_channels=mid_ch, kernel_size=kernel_size, stride=stride, padding=0, groups=groups1)),
+            ('conv1', nn.Conv2d(in_channels=in_ch, out_channels=mid_ch, kernel_size=kernel_size, padding=0)),
             ('norm1', nn.BatchNorm2d(mid_ch)),
             ('relu1', nn.ReLU(mid_ch)),
-            ('conv2', nn.Conv2d(in_channels=mid_ch, out_channels=out_ch, kernel_size=kernel_size, stride=stride, padding=0, groups=groups2)),
+            ('conv2', nn.Conv2d(in_channels=mid_ch, out_channels=out_ch, kernel_size=kernel_size, padding=0)),
             ('norm2', nn.BatchNorm2d(out_ch)),
             ('relu2', nn.ReLU(out_ch))
 
         ]))
 
     def forward(self, x):
+      """
+      Function to add each convolutional layers
+      """
         return self.conv_layers(x)
 
 
 class Down(nn.Module):
+  """
+  Down Convolution Block
+  """
 
-    def __init__(self, in_ch, mid_ch, out_ch, kernel_size, stride, groups1):
+    def __init__(self, in_ch, mid_ch, out_ch, kernel_size):
         super().__init__()
         self.maxpool_conv = nn.Sequential(OrderedDict([
             ('Maxpooling', nn.MaxPool2d(kernel_size=2, stride=2)),
-            ('Conv_layers', Network(in_ch, mid_ch, out_ch, kernel_size=kernel_size, stride=stride, groups1=groups1, groups2=groups1))
+            ('Conv_layers', Network(in_ch, mid_ch, out_ch, kernel_size))
 
         ]))
 
@@ -46,12 +56,15 @@ class Final_Up(nn.Module):
 
 
 class Up(nn.Module):
+  """
+  Up Convolution Block
+  """
 
-    def __init__(self, in_ch, mid_ch, out_ch, kernel_size, stride, groups1, bilinear=True):
+    def __init__(self, in_ch, mid_ch, out_ch, kernel_size, bilinear=True):
         super().__init__()
 
         self.up = nn.ConvTranspose2d(in_ch, out_ch, kernel_size=2, stride=2)
-        self.conv = Network(in_ch, mid_ch, out_ch, kernel_size=kernel_size, stride=stride, groups1=groups1, groups2=groups1)
+        self.conv = Network(in_ch, mid_ch, out_ch, kernel_size)
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
@@ -68,6 +81,9 @@ class Up(nn.Module):
 
 
 class Output(nn.Module):
+  ''' 
+  Output block
+  '''
 
     def __init__(self, in_ch, out_ch):
         super(Output, self).__init__()
@@ -80,31 +96,42 @@ class Output(nn.Module):
     def forward(self, x):
         return self.end(x)
 
-
+# Unet Architecture 
 class unet(nn.Module):
-    def __init__(self, n_channels, n_classes, kernel_size, stride, groups, bilinear=True):
+  """
+   Implementation of U-Net
+
+   Args:
+   --------
+   n_channels (int): number of input channels
+   n_classes (int): number of output channels
+   kernal_size (int): size of kernal
+
+  """
+    def __init__(self, n_channels, n_classes, kernel_size, bilinear=True):
         super().__init__()
 
         self.n_channels = n_channels
         self.n_classes = n_classes
         self.bilinear = bilinear
         self.kernel_size = kernel_size
-        self.stride = stride
-        self.groups = groups
 
-        self.start = Network(n_channels, 64, 64, kernel_size, stride, groups1=1, groups2=groups)
-        self.down1 = Down(64, 128, 128, kernel_size, stride, groups)
-        self.down2 = Down(128, 256, 256, kernel_size, stride, groups)
-        self.down3 = Down(256, 512, 512, kernel_size, stride, groups)
-        self.down4 = Down(512, 1024, 1024, kernel_size, stride, groups)
-        self.up1 = Up(1024, 512, 512, kernel_size, stride, groups)
-        self.up2 = Up(512, 256, 256, kernel_size, stride, groups)
-        self.up3 = Up(256, 128, 128, kernel_size, stride, groups)
-        self.up4 = Up(128, 64, 64, kernel_size, stride, groups)
+        self.start = Network(n_channels, 64, 64, kernel_size)
+        self.down1 = Down(64, 128, 128, kernel_size)
+        self.down2 = Down(128, 256, 256, kernel_size)
+        self.down3 = Down(256, 512, 512, kernel_size)
+        self.down4 = Down(512, 1024, 1024, kernel_size)
+        self.up1 = Up(1024, 512, 512, kernel_size)
+        self.up2 = Up(512, 256, 256, kernel_size)
+        self.up3 = Up(256, 128, 128, kernel_size)
+        self.up4 = Up(128, 64, 64, kernel_size)
         self.out = Output(64, n_classes)
         self.Final = Final_Up(n_classes, n_classes)
 
     def forward(self, x):
+      '''
+      Forward Pass
+      '''
         x1 = self.start(x)
         x2 = self.down1(x1)
         x3 = self.down2(x2)
